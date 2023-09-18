a60f7856ece60465825dedd377890db25c57315b
co-ze <wonje1108@gmail.com>
2023-09-18 17:06:11 +0900

    first commit

---
+++ src/main/java/hanghackaton/horanedu/HoranEduApplication.java
@@ -0,0 +1,13 @@
+package hanghackaton.horanedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoranEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoranEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hanghackaton/horanedu/HoranEduApplicationTests.java
@@ -0,0 +1,13 @@
+package hanghackaton.horanedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoranEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
